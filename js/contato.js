5170d6f5a6e2060532a9812d8a2f6eef943f41e4
Lucas Paulo <lp152659@gmail.com>
2024-11-16 22:13:12 -0300

    Adição de conteúdo, estilização e responsividade/conexão com as redes sociais e email

---
+++ js/contato.js
@@ -0,0 +1,34 @@
+//Funcionalidade (abrir janela do mapa)
+const janelaMap = document.getElementById('janelaMapa');
+function openMap(){
+    janelaMap.classList.add('ativeMap');
+}
+janelaMap.addEventListener('click', (element) => {
+    if(element.target.id === 'janelaMapa' || element.target.id === 'close'){
+        janelaMap.classList.remove('ativeMap');
+    }
+});
+
+//Funcionalidades de estilo card de redes sociais
+const social = document.getElementById('socialMedias');
+const iconMedia = document.querySelectorAll('.iconMedia');
+iconMedia.forEach((element) => {
+    element.addEventListener('mouseenter', (rede) =>{
+        console.log(rede.target.id);
+        if(rede.target.id === 'whatsapp'){
+            social.classList.add('styleWhats');
+            social.classList.remove('styleInsta');
+            social.classList.remove('styleFace');
+        }
+        if(rede.target.id === 'instagram'){
+            social.classList.add('styleInsta');
+            social.classList.remove('styleWhats');
+            social.classList.remove('styleFace');
+        }
+        if(rede.target.id === 'facebook'){
+            social.classList.add('styleFace');
+            social.classList.remove('styleInsta');
+            social.classList.remove('styleWhats');
+        }
+    });
+});
